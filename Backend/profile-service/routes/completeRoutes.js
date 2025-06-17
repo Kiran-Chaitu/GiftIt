25f21904104592ff6c747116c95a09e308e70850
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-06-17 11:17:23 +0530

    Delete Backend/profile-service directory

--- Backend/profile-service/routes/completeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { userProfileRouter } = require('./UserProfileRoutes');
-
-const routes = express.Router();
-
-routes.use(userProfileRouter);
-
-
-module.exports = { routes };
