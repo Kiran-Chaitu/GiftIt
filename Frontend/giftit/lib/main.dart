3c575e23bc8ec74d46064698b065fe01a368d069
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-06-18 00:36:16 +0530

    Add Internet Connectivity Checker

--- Frontend/giftit/lib/main.dart
+++ Frontend/giftit/lib/main.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:giftit/bloc/NGO/ngo_bloc.dart';
 import 'package:giftit/bloc/bottom_bar_cubit.dart';
 import 'package:giftit/configs/routes/route_names.dart';
 import 'package:giftit/configs/routes/routes.dart';
 import 'package:giftit/configs/themes/app_theme.dart';
+import 'package:giftit/controller/internet_connectivity_checker.dart';
 import 'package:giftit/repository/ngo_repository.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "assets/.env");
+  Get.put(InternetConnectivityController());
   runApp(const MyApp());
 }
 
@@ -29,7 +33,7 @@ class MyApp extends StatelessWidget {
             BlocProvider(create: (_) => BottomBarCubit()),
             BlocProvider(create: (_) => NgoBloc(repository: NGORepository()))
           ],
-          child: MaterialApp(
+          child: GetMaterialApp(
             debugShowCheckedModeBanner: false,
             theme: AppTheme.appTheme,
             initialRoute: RoutesNames.home,
